d16399ceb264f65757fe5fe64fa75506a1b811e8
marcos <marcos@Vostro-3450>
2018-04-12 19:48:55 -0300

    Inicio

---
+++ src/pooExercicio1/PooExercicio1.java
@@ -0,0 +1,34 @@
+/*
+Implementar um projeto de uma insituição de ensino, seguindo a descrição:
+
+INSTITUIÇÃO
+* Oferece cursos de 3 áreas: Engenharia, Administração e Design;
+* Cada área possui 2 cursos;
+* Cada curso possui 6 disciplinas;
+* Cada disciplina é aberta para até 20 alunos;
+* Cda aluno pode cursar até 3 disciplinas por período;
+
+PESSOAS
+* A instituição é composta por técnicos administrativos, professores e funcionários da limpeza;
+* As coordenações de cursos são de professores;
+* As secretarias (educação, financeira, administração), possuem 1 chefe e 1 secretário, ambos técnicos administrativos.
+* Os alunos fazem parte dos recursos humanos gerenciados.
+
+OBS: ao definir uma hierarquia de herança defir construtores que validem os dados do objeto.
+ */
+package pooExercicio1;
+
+/**
+ *
+ * @author marcos
+ */
+public class PooExercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
